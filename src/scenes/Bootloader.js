186272d23ae047831b45bfee1ddd025c4d03d5db
DavidSC <david@wbcboxing.com>
2021-09-27 13:20:47 -0500

    practica bloqueo de raton

--- src/scenes/Bootloader.js
+++ src/scenes/Bootloader.js
@@ -13,72 +13,31 @@ class Bootloader extends Phaser.Scene {
     }
 
     create() {
-        this.cubix = this.add.image(100, 100, 'cubix').setInteractive();
-        this.cubix_fondo = this.add.image(100, 200, 'cubix_fondo').setInteractive();
-        const eventos = Phaser.Input.Events;
-        // console.log(eventos);
+        this.cubix = this.add.image(100, 100, 'cubix');
+        const events = Phaser.Input.Events;
+        const keyboard = Phaser.Input.Keyboard;
 
-        this.input.on(eventos.POINTER_DOWN, (evento) => {
-            console.log("Se ha clicado en el canvas");
-            // console.log(evento);
+        //Ocultar o bloquear el mouse en el canvas
+        this.input.on(events.POINTER_DOWN, () => {
+            this.input.mouse.requestPointerLock();
         });
-        this.input.on(eventos.POINTER_UP, (evento) => {
-            console.log("Se ha levantado el puntero  en el canvas");
 
-        });
-        this.input.on(eventos.POINTER_MOVE, (evento) => {
-            // console.log("Se ha movido el puntero  en el canvas");
-            if (evento.isDown) {
+        this.input.on(events.POINTER_MOVE, (pointer) => {
+            // this.cubix.x = pointer.worldX; //->solo funciona cuando no se bloquea el mouse
 
-                this.cubix.x = evento.worldX;
-                this.cubix.y = evento.worldY;
+            //Se suma ya que avanza en coordenadas del canvas, si no se quedaria en lugar fijo
+            if (this.input.mouse.locked) {
+
+                this.cubix.x += pointer.movementX;
+                this.cubix.y += pointer.movementY;
             }
-
         });
-        //Eventos para saber si estoy fuera del canvas
+        //Desbloquea el raton presionando la tecla A
+        this.input.keyboard.addKey(keyboard.KeyCodes.A)
+            .on('down', () => {
+                this.input.mouse.releasePointerLock();
+            });
 
-        this.input.on(eventos.GAME_OVER, () => {
-            console.log('has entrado en el lienzo');
-        });
-        this.input.on(eventos.GAME_OUT, () => {
-            console.log('has salido en el lienzo');
-        });
-
-        //pARA SABER SI SE da click fuera del canvas
-        this.input.on(eventos.POINTER_DOWN_OUTSIDE, () => {
-            console.log('HAZ CLICADO FUERA DEL LIENZCO');
-        });
-        this.input.on(eventos.POINTER_UP_OUTSIDE, () => {
-            console.log('HAS LEVANTADO FUERA DEL LIENZO');
-        });
-
-        //Para hacer los objetpos del lienzco interactivos, poner
-        //setInteractive()
-
-        // this.input.on(eventos.GAMEOBJECT_DOWN, (pointer, gameObject) => {
-        //     gameObject.setTint(0x00ff00);
-        // });
-        // this.input.on(eventos.GAMEOBJECT_UP, (pointer, gameObject) => {
-        //     gameObject.clearTint();
-        // });
-
-        //UN OVER EN EL OBJECTO
-
-        // this.input.on(eventos.GAMEOBJECT_OVER, (pointer, gameObject) => {
-        //     gameObject.setTint(0x0000ff);
-        // });
-        // this.input.on(eventos.GAMEOBJECT_OUT, (pointer, gameObject) => {
-        //     gameObject.clearTint();
-        // });
-
-
-        //-->se usa function(){} para que funcione el this
-        this.cubix.on(eventos.POINTER_DOWN, function() {
-            this.setTint(0x00ff00);
-        });
-        this.cubix.on(eventos.POINTER_UP, function() {
-            this.clearTint();
-        });
 
 
     }
