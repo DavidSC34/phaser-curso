fdeaf16f33511de9a99fa0536130014c720cc51f
DavidSC <david@wbcboxing.com>
2021-10-01 00:53:55 -0500

    usando atlas y amins tools para animar

--- src/scenes/Bootloader.js
+++ src/scenes/Bootloader.js
@@ -10,36 +10,31 @@ class Bootloader extends Phaser.Scene {
     preload() {
         this.load.path = './assets/';
         this.load.image(['cubix', 'cubix_fondo']);
-        this.load.spritesheet('tomato', 'tomato/tomato.png', {
-            frameWidth: 16,
-            frameHeight: 25,
-        }); //-->tamaño del recorte en la imagen
-        // this.load.spritesheet('tomato_spacing', 'tomato_spacing/tomato_spacing.png', {
-        //     frameWidth: 16,
-        //     frameHeight: 25,
-        //     margin: 1,
-        //     spacing: 2
-        // }); //-->tamaño del recorte en la imagen
+        //Ya que usamos una herramienta para generar atlas y animacion, la usamos
+        this.load.atlas('tomato', 'tomato/tomato.png', 'tomato/tomato_atlas.json');
+        this.load.animation('tomatoAnim', 'tomato/tomato_anim.json');
     }
 
     create() {
-        // this.cubix = this.add.image(100, 100, 'cubix');
-        this.tomato = this.add.sprite(100, 100, 'tomato', 2).setScale(4);
-        // this.tomato_spacing = this.add.sprite(100, 200, 'tomato_spacing', 2).setScale(4);
-        //Crar un animacion
 
-        this.anims.create({
-            key: 'tomato_walk',
-            frames: this.anims.generateFrameNumbers('tomato', {
-                // start: 0,
-                // end: 7
-                frames: [0, 1, 2, 3, 4, 5, 6, 7]
-            }),
-            repeat: -1,
-            frameRate: 15
-        });
+        this.tomato = this.add.sprite(100, 100, 'tomato').setScale(4);
+        this.tomato.anims.play('tomato_walk'); //-->nombre que esta en el json
+        //Crear un animacion, forma sin usar la herramienta de generar animaciones
 
-        this.tomato.anims.play('tomato_walk');
+        // this.anims.create({
+        //     key: 'tomato_walk',
+        //     frames: this.anims.generateFrameNames('tomato', {
+        //         // suffix: '.png',
+        //         prefix: 'tomato_animation_1_',
+        //         start: 0,
+        //         end: 7,
+
+        //     }),
+        //     repeat: -1,
+        //     frameRate: 15
+        // });
+
+        // this.tomato.anims.play('tomato_walk');
         // this.anims.play('tomato_walk', this.tomato);//--> forma alterna de animar
 
     }
