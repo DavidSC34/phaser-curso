438a29c946f51be1abef0e85b3bab0f9c6a470dc
DavidSC <david@wbcboxing.com>
2021-10-06 02:42:57 -0500

    uso de contenedores

--- src/scenes/Bootloader.js
+++ src/scenes/Bootloader.js
@@ -10,32 +10,41 @@ class Bootloader extends Phaser.Scene {
     preload() {
         this.load.path = './assets/';
         this.load.image(['cubix', 'cubix_fondo']);
-        //cargar la fuente(imagen)
-        this.load.image('fuente', 'fuentes/fuente.png');
-        this.load.json('fuente_json', 'fuentes/fuente.json'); //--> solo lo carga en memoria
+        this.load.image('contenedor', 'contenedor.png');
+
 
     }
 
     create() {
-        const fuenteConfig = this.cache.json.get('fuente_json');
-        console.log(fuenteConfig);
-        this.cache.bitmapFont.add('nombreFuente', Phaser.GameObjects.RetroFont.Parse(this, fuenteConfig)); //-->nombre interno para llamar la fuente
+        this.cubix = this.add.image(300, 100, 'cubix')
+            .setScale(0.7)
+            .setDepth(2); //--> este valor lo ignora el orden en el contenedor
+        this.contenedor = this.add.image(200, 100, 'contenedor');
+        this.texto = this.add.text(90, 90, 'PUNTOS 0', {
+            fontSize: 30
+        });
 
-        this.texto = this.add.bitmapText(100, 100, 'nombreFuente', ' PUNTOS 0', 30, 0).setTint(0xffEE00); //-->ultimo parametro  es alineacion (0:izq, 1:centrado,2:derecha)
-
-        //-->cambiar de color se pone tinte
-        // this.texto.setText('Soy el nuevo texto');//--> no sabe de minusculas
-        // this.texto.setText('SOY EL NUEVO TEXTO');
-        this.puntos = 0;
-
+        const container = this.add.container(0, -300); //--> coordenas en la pantalla container
+        container.add([
+            this.contenedor,
+            this.cubix, //--> el valor setDepth(2), es ignorado aqui
+            this.texto
+        ]);
+        // container.setScale(2)
+        //--> Colocar todos los objetos en un contenedor y aplicar el tween
+        //una animacion a todo el contenido de
+        this.add.tween({
+            targets: [container],
+            ease: 'Bounce',
+            y: 100
+        });
 
     }
 
 
 
     update(time, delta) {
-        // this.puntos++;
-        // this.texto.setText('PUNTOS ' + this.puntos);
+
     }
 }
 
